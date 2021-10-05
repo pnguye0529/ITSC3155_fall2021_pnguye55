e877f18e18b93f88da9734a30f6315a8fd693059
Po Nguyen <pnguye55@uncc.edu>
2021-10-05 09:32:55 -0400

    PythonBasics2 - Update methods

--- PythonBasics2/pythonBasics2.py
+++ PythonBasics2/pythonBasics2.py
@@ -6,55 +6,71 @@
 
 
 # Part A. count_threes
-# Define a function count_threes(n) that takes an int and
-# returns the number of multiples of 3 in the range from 0
-# to n (including n).
+# needs to return the multiple of three that occurs the most in a string.
+# For example, 0939639 would return 9 since it appeared 3 times while the
+# other multiple of three appeared less than that. You only need to worry
+# about single digit multiples of 3 (3, 6, 9). You must use a dictionary
 
 def count_threes(n):
-    # if the number is divisible by 3 then return the division n/3
-    if n % 3 == 0:
-        return int(n / 3)
-    # if not, return 0
-    return 0
+    # new dict
+    d = dict()
+    # traverse through the string n
+    for i in n:
+        # if current char is already a key in the dictionary, then increase the value of the key
+        if i in d:
+            d[i] += 1
+        # if not, then set the current value of the key to 1
+        else:
+            d[i] = 1
+    occ = max(d, key=d.get)
+    return int(occ)
 
 
-# Part B. longest_consecutive_repeating_char
-# Define a function longest_consecutive_repeating_char(s) that takes
-# a string s and returns the character that has the longest consecutive repeat.
+# longest_consecutive_repeating_char needs to account for the edge case where two characters have the same consecutive repeat length.
+# The return value should now be a list containing all characters with the longest consecutive repeat.
+# For example, the longest_consecutive_repeating_char('aabbccd') would return ['a', 'b', 'c'] (order doesn't matter).
 def longest_consecutive_repeating_char(s):
-    s_length = len(s)
-    counter = 0
-    temp_count = 1
-    character = s[0]
-    for i in range(s_length):
-        for j in range(i+1, s_length):
-            if(s[i] != s[j]):
-                break
-            temp_count += 1
+    # create a new dictionary
+    d = dict()
+    # create variable count and temp char
+    count = 0
+    temp_char = ''
+    # traverse through the string
+    for char in s:
+        # if the current character in s is = to temp
+        if char == temp_char:
+            # then the character is already seen, thus add 1 to count
+            count += 1
+            # then update the count for the current key
+            d[char] = count
+        # if the current char is not = to temp, then set the count to 1 since it is first seen
+        else:
+            count = 1
+            # then set the temp character to current char
+            temp_char = char
+    # create a var ret that hold the max value within the dict
+    ret = max(d, key=d.get)
+    # return
+    return ret
 
-        if temp_count > counter:
-            counter = temp_count
-            character = s[i]
-    return character
 
-
-# Part C. is_palindrome
-# Define a function is_palindrome(s) that takes a string s
-# and returns whether or not that string is a palindrome.
-# A palindrome is a string that reads the same backwards and
-# forwards. Treat capital letters the same as lowercase ones
-# and ignore spaces (i.e. case insensitive).
-def is_palindrome(s):
-    # remove all white space of string s and store it to the new variable str
-    str = s.replace(" ", "")
-    # declare a new variable tail and assign the last index of string s to it
-    tail = len(str) - 1
-    # start the loop from the first character to the middle character in the string s
-    for i in range(0, int(len(str) / 2)):
-        # compare the head and the tail character ignore the case
-        # using lower() function to compare the characters
-        if str[i].lower() != (str[tail - i].lower()):
-            # return false if any of the pair does not match
-            return False
-    # return true
-    return True
+# # Part C. is_palindrome
+# # Define a function is_palindrome(s) that takes a string s
+# # and returns whether or not that string is a palindrome.
+# # A palindrome is a string that reads the same backwards and
+# # forwards. Treat capital letters the same as lowercase ones
+# # and ignore spaces (i.e. case insensitive).
+# def is_palindrome(s):
+#     # remove all white space of string s and store it to the new variable str
+#     str = s.replace(" ", "")
+#     # declare a new variable tail and assign the last index of string s to it
+#     tail = len(str) - 1
+#     # start the loop from the first character to the middle character in the string s
+#     for i in range(0, int(len(str) / 2)):
+#         # compare the head and the tail character ignore the case
+#         # using lower() function to compare the characters
+#         if str[i].lower() != (str[tail - i].lower()):
+#             # return false if any of the pair does not match
+#             return False
+#     # return true
+#     return True
