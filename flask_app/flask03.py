0e0c12f4391daae456c8e5837f5c81606460d795
Po Nguyen <pnguye55@uncc.edu>
2021-10-26 09:08:07 -0400

    flask app #3 - aded execution path for POST request for new note

--- flask_app/flask03.py
+++ flask_app/flask03.py
@@ -4,6 +4,7 @@
 import os                 # os is used to get environment variables IP & PORT
 from flask import Flask   # Flask is the web app that we will customize
 from flask import render_template
+from flask import request
 app = Flask(__name__)     # create an app
 
 # @app.route is a decorator. It gives the function "index" special powers.
@@ -17,20 +18,26 @@ def index():
 
 @app.route('/notes')
 def get_notes():
+    a_user = {'name': 'Po Nguyen', 'email': 'pnguye55@uncc.edu'}
     notes = {1: {'title': 'First note', 'text': 'This is my first note', 'date': '10-1-2020'},
-             2: {'title': 'First note', 'text': 'This is my first note', 'date': '10-1-2020'}}
-    return render_template('notes.html', notes=notes)
+             2: {'title': 'Second note', 'text': 'This is my second note', 'date': '10-2-2020'},
+             3: {'title': 'Third note', 'text': 'This is my third note', 'date': '10-3-2020'}}
+    return render_template('notes.html', notes=notes, user=a_user)
 
 @app.route('/notes/<note_id>')
 def get_note(note_id):
     notes = {1: {'title': 'First note', 'text': 'This is my first note', 'date': '10-1-2020'},
              2: {'title': 'Second note', 'text': 'This is my second note', 'date': '10-2-2020'},
              3: {'title': 'Third note', 'text': 'This is my third note', 'date': '10-3-2020'}}
-    return render_template('note.html', note=notes[int(note_id)])
+    a_user = {'name': 'Po Nguyen', 'email': 'pnguye55@uncc.edu'}
+    return render_template('note.html', note=notes[int(note_id)], user=a_user)
 
-@app.route('/notes/new')
+@app.route('/notes/new', methods=['GET', 'POST'])
 def new_note():
     a_user = {'name': 'Po Nguyen', 'email': 'pnguye55@uncc.edu'}
+
+    if request.method == 'POST':
+        return '<h1> POST method used for this request</h1>'
     return render_template('new.html', user=a_user)
 
 app.run(host=os.getenv('IP', '127.0.0.1'),port=int(os.getenv('PORT', 5000)),debug=True)
