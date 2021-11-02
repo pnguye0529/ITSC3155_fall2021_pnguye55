9e77cb462fba16a25c85f53de0d68a3e9f03e05e
Po Nguyen <pnguye55@uncc.edu>
2021-11-02 17:05:35 -0400

    flask app #4 - fixing some errors

--- flask_app/flask04.py
+++ flask_app/flask04.py
@@ -12,16 +12,17 @@ from models import Note as Note
 from models import User as User
 
 app = Flask(__name__)     # create an app
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flask_note_app.db'
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 #  Bind SQLAlchemy db object to this Flask app
 db.init_app(app)
+
 # Setup models
 with app.app_context():
     db.create_all()   # run under the app context
-
-a_user =  db.session.query(User).filter_by(email='pnguye55@uncc.edu')
-
 
 # @app.route is a decorator. It gives the function "index" special powers.
 # In this case it makes it so anyone going to "your-url/" makes this function
@@ -29,17 +30,20 @@ a_user =  db.session.query(User).filter_by(email='pnguye55@uncc.edu')
 @app.route('/')
 @app.route('/index')
 def index():
+    a_user = db.session.query(User).filter_by(email='pnguye55@uncc.edu')
     return render_template('index.html', user=a_user)
 
 @app.route('/notes')
 def get_notes():
+    a_user = db.session.query(User).filter_by(email='pnguye55@uncc.edu')
     notes = db.session.query(Note).all()
     return render_template('notes.html', notes=notes, user=a_user)
 
 @app.route('/notes/<note_id>')
 def get_note(note_id):
+    a_user = db.session.query(User).filter_by(email='pnguye55@uncc.edu')
     note = db.session.query(Note).filter_by(id=note_id)
-    return render_template('note.html', note=notes[int(note_id)], user=a_user)
+    return render_template('note.html', note=note, user=a_user)
 
 @app.route('/notes/new', methods=['GET', 'POST'])
 def new_note():
@@ -53,6 +57,7 @@ def new_note():
         db.session.commit()
         return redirect(url_for('get_notes'))
     else:
+        a_user = db.session.query(User).filter_by(email='pnguye55@uncc.edu')
         return render_template('new.html', user=a_user)
 
 app.run(host=os.getenv('IP', '127.0.0.1'),port=int(os.getenv('PORT', 5000)),debug=True)
