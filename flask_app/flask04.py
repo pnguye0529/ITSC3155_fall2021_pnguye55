82b4033a78ec5aacf82b185f740e3ea57404c78a
Po Nguyen <pnguye55@uncc.edu>
2021-11-03 09:22:05 -0400

    flask app #4 - db queries

--- flask_app/flask04.py
+++ flask_app/flask04.py
@@ -1,8 +1,8 @@
 # FLASK Tutorial 1 -- We show the bare bones code to get an app up and running
 
 # imports
-import os  # os is used to get environment variables IP & PORT
-from flask import Flask  # Flask is the web app that we will customize
+import os                 # os is used to get environment variables IP & PORT
+from flask import Flask   # Flask is the web app that we will customize
 from flask import render_template
 from flask import request
 from flask import redirect, url_for
@@ -11,19 +11,15 @@ from database import db
 from models import Note as Note
 from models import User as User
 
-app = Flask(__name__)  # create an app
 
+app = Flask(__name__)     # create an app
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flask_note_app.db'
-
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 #  Bind SQLAlchemy db object to this Flask app
 db.init_app(app)
-
 # Setup models
 with app.app_context():
-    db.create_all()  # run under the app context
-
+    db.create_all()   # run under the app context
 
 # @app.route is a decorator. It gives the function "index" special powers.
 # In this case it makes it so anyone going to "your-url/" makes this function
@@ -31,23 +27,20 @@ with app.app_context():
 @app.route('/')
 @app.route('/index')
 def index():
-    a_user = db.session.query(User).filter_by(email='pnguye55@uncc.edu').one()
+    a_user = db.session.query(User).filter_by(email='pnguye55@uncc.edu')
     return render_template('index.html', user=a_user)
-
 
 @app.route('/notes')
 def get_notes():
-    a_user = db.session.query(User).filter_by(email='pnguye55@uncc.edu').one()
+    a_user = db.session.query(User).filter_by(email='pnguye55@uncc.edu')
     notes = db.session.query(Note).all()
     return render_template('notes.html', notes=notes, user=a_user)
 
-
 @app.route('/notes/<note_id>')
 def get_note(note_id):
-    a_user = db.session.query(User).filter_by(email='pnguye55@uncc.edu').one()
-    note = db.session.query(Note).filter_by(id=note_id).one()
+    a_user = db.session.query(User).filter_by(email='pnguye55@uncc.edu')
+    note = db.session.query(Note).filter_by(id=note_id)
     return render_template('note.html', note=note, user=a_user)
-
 
 @app.route('/notes/new', methods=['GET', 'POST'])
 def new_note():
@@ -59,13 +52,13 @@ def new_note():
         new_record = Note(title, text, today)
         db.session.add(new_record)
         db.session.commit()
+
         return redirect(url_for('get_notes'))
     else:
-        a_user = db.session.query(User).filter_by(email='pnguye55@uncc.edu').one()
+        a_user = db.session.query(User).filter_by(email='pnguye55@uncc.edu')
         return render_template('new.html', user=a_user)
 
-
-app.run(host=os.getenv('IP', '127.0.0.1'), port=int(os.getenv('PORT', 5000)), debug=True)
+app.run(host=os.getenv('IP', '127.0.0.1'),port=int(os.getenv('PORT', 5000)),debug=True)
 
 # To see the web page in your web browser, go to the url,
 #   http://127.0.0.1:5000
